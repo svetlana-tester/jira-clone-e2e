5276537f596bc7cc0bfb2359a5fd621bfc0cbfe4
svetlana-tester <svetlanaobrezkova.qa@gmail.com>
2023-07-15 17:09:41 +0300

    assignment 3 and 4

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -17,9 +17,17 @@ describe('Issue delete', () => {
 
   it('Should delete issue successfully', () => {
     //add steps to delete issue
+IssueModal.clickDeleteButton();
+IssueModal.confirmDeletion();
+IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+
   });
 
   it('Should cancel deletion process successfully', () => {
     //add steps to start deletion proces but cancel it
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal. closeDetailModal();
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
   });
 });
--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,44 +1,80 @@
-describe('Deleting an issue', () => {
-  let getIssueDetailsModal;
+
+
+describe('Issue deletion suite', () => {
+  let confirmationModal;
 
   beforeEach(() => {
     cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
-      cy.visit(url + '/board');
-      // open first issue from the board
-      cy.contains('This is an issue of type: Task.').click();
-      getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
-    });
+    cy.url()
+      .should('eq', `${Cypress.env('baseUrl')}project`)
+      .then((url) => {
+        cy.visit(url + '/board');
+        cy.get('[data-testid="board-list:backlog"]')
+          .should('be.visible')
+          .and('have.length', '1').within(() => {
+            cy.get('[data-testid="list-issue"]')
+              .should('have.length', '4')
+              // open the first issue
+              .first()
+              .click();
+          })
+          // issue detail modal is visible
+        cy.get('[data-testid="modal:issue-details"]').should('be.visible');
+      });
+      // click 'delete' button
+      cy.get('[data-testid="icon:trash"]')
+      .click();
+      confirmationModal = cy.get('[data-testid="modal:confirm"]');
+      confirmationModal.should('be.visible')
+      .and('contain', 'Are you sure you want to delete this issue?');
   });
 
-  it('Test case deletes an issue and verifies its absence on the Jira board', () => {
 
-    // assert that issue detail view modal is visible
-    getIssueDetailsModal().within(() => {
-      cy.contains('This is an issue of type: Task.')
-      // click delete button
-      cy.get('[data-testid="icon:trash"]').click();
-
-
-
-    })
-    // const for confirmation window
-    const confirmationModal = cy.get('[data-testid="modal:confirm"]');
-    confirmationModal.should('be.visible');
-    confirmationModal.should('contain', 'Are you sure you want to delete this issue?');
+  it('Test 1: deletes first issue from the board', () => {
 
     confirmationModal.within(() => {
       // confirm deletion
-      cy.contains('Delete issue').trigger('mouseover')
-        .trigger('click');
+      cy.contains('button', 'Delete issue')
+        .click();
 
     })
 
-    // deletion confirmation dialogue is not visible
+    // confirmation pop-up is not visible
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+
+    // issue has been deleted from the board
+    cy.reload();
+    cy.get('[data-testid="board-list:backlog"]')
+      .should('be.visible')
+      .and('have.length', '1')
+      .within(() => {
+        cy.get('[data-testid="list-issue"]').should('have.length', '3')
+      })
+
+  })
 
 
-    //issue is deleted and not displayed on the Jira board anymore.
-  });
+  it('Test 2: cancels issue deletion', () => {
+    confirmationModal.within(() => {
+      // cancel the deletion
+      cy.contains('button','Cancel').click();
+    });
+  
+    // confirmation pop-up not visible
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+  
+    cy.get('[data-testid="icon:close"]').first().click();
+    cy.get('[data-testid="modal:issue-details"]').should('not.exist');
+    cy.reload();
+
+  
+    // issue is still displayed on the board
+    cy.get('[data-testid="board-list:backlog"]')
+      .should('be.visible')
+      .and('have.length', '1')
+      .within(() => {
+        cy.get('[data-testid="list-issue"]').should('have.length', '4');
+      })
+  })
+  
 })
-
-
