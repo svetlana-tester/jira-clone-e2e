2e4f6b6906068ed7260fb67be747779d6ea68887
svetlana-tester <svetlanaobrezkova.qa@gmail.com>
2023-07-28 19:33:11 +0300

    small corrections

--- cypress/e2e/e2e-pom/issue-comments-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-comments-in-pom.cy.js
@@ -1,23 +1,24 @@
-import { fa } from 'faker/lib/locales';
 import IssueModal from '../../pages/IssueModal';
 
-const issueTitle = 'This is an issue of type: Task.'
+const issueTitle = 'This is an issue of type: Task.';
 
 describe('Issue comments creating, editing and deleting', () => {
   beforeEach(() => {
-      cy.visit('/');
-      cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-          cy.visit(url + '/board');
-          cy.contains(issueTitle).click();
+    cy.visit('/');
+    cy.url()
+      .should('eq', `${Cypress.env('baseUrl')}project/board`)
+      .then((url) => {
+        cy.visit(url + '/board');
+        cy.contains(issueTitle).click();
       });
   });
-  
+
   it('Should create, edit and delete a comment', () => {
-    const addNewComment = 'A new test comment'
-    const editNewComment = 'Changing a test comment'
+    const addNewComment = 'A new test comment';
+    const editNewComment = 'Changing a test comment';
     IssueModal.addComment(addNewComment);
     IssueModal.editComment(addNewComment, editNewComment);
     IssueModal.deleteComment();
     IssueModal.confirmEditedCommentDeletion(editNewComment);
-})
-})
+  });
+});
