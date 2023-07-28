f7cc910130f7943aa6de954135655736381dd57b
svetlana-tester <svetlanaobrezkova.qa@gmail.com>
2023-07-28 19:30:21 +0300

    added Nadezda's solution

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -55,6 +55,37 @@ describe('Issue comments creating, editing and deleting', () => {
         });
     });
 
+    it('Should create,edit and delete comment successfully', () => { // the correct solution by Nadezda
+        const comment = 'TEST_COMMENT';
+        const comment_edited = 'TEST_COMMENT_EDITED';
+      
+        getIssueDetailsModal().within(() => {
+            //add comment
+            cy.contains('Add a comment...').click();
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+            cy.contains('button', 'Save').click().should('not.exist');
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+      
+            //edit comment
+            cy.get('[data-testid="issue-comment"]').first().contains('Edit')
+                .click().should('not.exist');
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', comment).clear().type(comment_edited);
+            cy.contains('button', 'Save').click().should('not.exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', 'Edit')
+                .and('contain', comment_edited);
+      
+            //delete comment
+            cy.contains('Delete').click();
+        });
+      
+            cy.get('[data-testid="modal:confirm"]').contains('button', 'Delete comment')
+                .click().should('not.exist');
+            getIssueDetailsModal().contains(comment_edited).should('not.exist');
+      
+      });
+
     it('Should delete a comment successfully', () => {
         getIssueDetailsModal()
             .find('[data-testid="issue-comment"]')
